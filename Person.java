0ac5b08c6509f9961d3c09e2f878954e74491bbb
MoonfallMan <150876027+MoonfallMan@users.noreply.github.com>
2024-04-16 18:47:36 -0700

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,23 @@
+public class Person {
+	private String name;
+	private int id;
+
+	public Person(String name, int id) {
+		this.name = name;
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", id=" + id + "]";
+	}
+
+}
